c7a6650369f8ec2249e2b0aafe2be3684978b5e7
yujohnnyzhou <johnny0614@gmail.com>
2014-03-25 21:09:21 +1030

    mobile section slide down
    mobile section slide down

--- js/mobile.js
+++ js/mobile.js
@@ -6,7 +6,14 @@
 (function($){
 
 	$("a.arrow").on('tap',function(){
-		
+		var arrow = $(this);
+		arrow.animate({
+			'-moz-transform':'rotate('+180+'deg)',
+			'-ms-transform' : 'rotate('+180+'deg)',
+			'-webkit-transform' : 'rotate('+180+'deg)',
+			'transform' : 'rotate('+180+'deg)'
+		})
+		.parent().siblings('p').slideDown();
 	});
 
 })(jQuery);
