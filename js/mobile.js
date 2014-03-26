bef7255a63ac6c0e15996fa50b0cc6f3a370c6a7
yujohnnyzhou <johnny0614@gmail.com>
2014-03-26 14:00:01 +1030

    mobile arrow upside-down and slide toggle
    mobile arrow upside-down and slide toggle

--- js/mobile.js
+++ js/mobile.js
@@ -5,15 +5,51 @@
 
 (function($){
 
+	$.fn.animateRotate = function(angle,duration) {
+		return this.each(function(){
+			var $elem = $(this);
+
+			$({deg : 0}).animate({deg : angle},{
+				duration : duration,
+				easing : 'linear',
+				step : function(now, fx) {
+					$elem.css({
+						'-moz-transform':'rotate('+180+'deg)',
+					'-ms-transform' : 'rotate('+180+'deg)',
+					'-webkit-transform' : 'rotate('+180+'deg)',
+					'transform' : 'rotate('+180+'deg)'
+
+					});
+				},
+				complete : function(){
+						   $elem.removeAttr('style');
+						   var $icon = $elem.children('span');
+						   if($icon.hasClass('glyphicon-chevron-down')) {
+							   $icon.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+						   } else {
+							   $icon.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+						   }
+					   }
+			});
+		});
+	};
+
+	$.fn.arrowUpAndDown = function() {
+		return this.each(function(){
+			var $icon = $(this).children('span');
+			if($icon.hasClass('glyphicon-chevron-down')) {
+				$icon.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+			} else {
+				$icon.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+			}
+
+		});
+	};
+
 	$("a.arrow").on('tap',function(){
 		var arrow = $(this);
-		arrow.animate({
-			'-moz-transform':'rotate('+180+'deg)',
-			'-ms-transform' : 'rotate('+180+'deg)',
-			'-webkit-transform' : 'rotate('+180+'deg)',
-			'transform' : 'rotate('+180+'deg)'
-		})
-		.parent().siblings('p').slideDown();
+		arrow.arrowUpAndDown()
+		.parent().siblings('p').slideToggle();
 	});
 
 })(jQuery);
