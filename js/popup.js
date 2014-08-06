5a37747256f476e9cab5998d725f27999794f8b5
Yu Zhou <johnny0614@gmail.com>
2014-08-06 14:39:46 +0930

    iage media page popup window
    iage media page popup window

--- js/popup.js
+++ js/popup.js
@@ -3,4 +3,20 @@
 			e.preventDefault();
 			$('#iageweeklywindow').bPopup();
 		});
+	$('#businessweekly').bind('click', function(e){
+			e.preventDefault();
+			$('#businessweeklywindow').bPopup();
+		});
+	$('#propertyweekly').bind('click', function(e){
+			e.preventDefault();
+			$('#propertyweeklywindow').bPopup();
+		});
+	$('#sachinese').bind('click', function(e){
+			e.preventDefault();
+			$('#sachinesewindow').bPopup();
+		});
+	$('#sns').bind('click', function(e){
+			e.preventDefault();
+			$('#snswindow').bPopup();
+		});
 })(jQuery);
